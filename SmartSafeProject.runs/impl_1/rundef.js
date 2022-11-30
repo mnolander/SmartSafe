ec1b1671e88954e9790e4559f4fac215062b08b3
Michael Nolander <nolander.michael@gmail.com>
2022-11-29 22:30:43 -0800

    Added overview image

--- SmartSafeProject.runs/impl_1/rundef.js
+++ SmartSafeProject.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "write_bitstream", "begin" );
+ISETouchFile( "init_design", "begin" );
 ISEStep( "vivado",
          "-log SmartSafe.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source SmartSafe.tcl -notrace" );
 
